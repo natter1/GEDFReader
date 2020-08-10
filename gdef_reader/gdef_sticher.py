412b70bc49c984076a73eb63c46fedc874ebde9d
natter1 <njoehrmann@gmail.com>
2020-08-10 18:27:45 +0200

    update

--- gdef_reader/gdef_sticher.py
+++ gdef_reader/gdef_sticher.py
@@ -1,5 +1,6 @@
 import copy
 from pathlib import Path
+from typing import List
 
 import matplotlib.pyplot as plt
 # from scipy import misc
@@ -132,9 +133,9 @@ def main():
     #figure_gradient_rms.show()
 
 
-def stich(data01, data02, data01_x_offset):
+def _stich(data01, data02, data01_x_offset):
     """
-
+    Stiches two GDEFMeasurement.values using cross-correlation.
     :param data01:
     :param data02:
     :param data01_x_offset: used to specify max. overlap area, thus increasing speed and reducing risk of wrong stiching
@@ -150,21 +151,6 @@ def stich(data01, data02, data01_x_offset):
     y, x = np.unravel_index(np.nanargmax(reduced_correlation), reduced_correlation.shape)  # find (first) best match
     y, x = y - data02.shape[0] + 1, x + 1 + data01_x_offset  # - data02_x_offset_right)  # test with two identical datasets -> should give: y, x = 0, 0
 
-    fig, (ax_orig, ax_template, ax_corr, ax_stich) = plt.subplots(4, 1, figsize=(6, 20))
-
-    ax_orig.imshow(data01, cmap='gray')
-    ax_orig.set_title('data01')
-    ax_orig.set_axis_off()
-
-    ax_template.imshow(data02, cmap='gray')
-    ax_template.set_title('data02')
-    ax_template.set_axis_off()
-
-    ax_corr.imshow(correlation, cmap='gray')
-    ax_corr.set_title('Cross-correlation')
-    ax_corr.set_axis_off()
-
-    ax_orig.plot(x, y, 'ro')
 
     data01_x0 = - min(0, x)
     data01_y0 = - min(0, y)
@@ -177,22 +163,53 @@ def stich(data01, data02, data01_x_offset):
     data02_height = data02.shape[0] + data02_y0
     data02_width = data02.shape[1] + data02_x0
 
-    data_stiched = np.full([max(data01_height, data02_height), max(data01_width, data02_width)], np.nan)
+    result = np.full([max(data01_height, data02_height), max(data01_width, data02_width)], np.nan)
 
-    data_stiched[data01_y0:data01_height, data01_x0:data01_width] = data01
-    data_stiched[data02_y0:data02_height, data02_x0:data02_width] = data02
+    result[data01_y0:data01_height, data01_x0:data01_width] = data01
+    result[data02_y0:data02_height, data02_x0:data02_width] = data02
 
-    ax_stich.set_title('stiched')
-    ax_stich.set_axis_off()
-    ax_stich.imshow(data_stiched, cmap='gray')
+    # fig, (ax_orig, ax_template, ax_corr, ax_stich) = plt.subplots(4, 1, figsize=(6, 20))
+    #
+    # ax_orig.imshow(data01, cmap='gray')
+    # ax_orig.set_title('data01')
+    # ax_orig.set_axis_off()
+    #
+    # ax_template.imshow(data02, cmap='gray')
+    # ax_template.set_title('data02')
+    # ax_template.set_axis_off()
+    #
+    # ax_corr.imshow(correlation, cmap='gray')
+    # ax_corr.set_title('Cross-correlation')
+    # ax_corr.set_axis_off()
+    #
+    # ax_orig.plot(x, y, 'ro')
 
-    fig.show()
+    # ax_stich.set_title('stiched')
+    # ax_stich.set_axis_off()
+    # ax_stich.imshow(result, cmap='gray')
+    #
+    # fig.show()
 
-    return data_stiched
+    return result
+
+
+def stich(values_list : List[np.ndarray], initial_x_offset_fraction = 0.35) -> np.ndarray:
+    """
+    Stiches a list of GDEFMeasurement.values using cross-correlation.
+    :param values_list: List of GDEFMeasurement.values
+    :param initial_x_offset_fraction: used to specify max. overlap area, thus increasing speed and reducing risk of wrong stiching
+    :return: stiched np.ndarray
+    """
+    result = values_list[0]
+    x_offset_right = round(values_list[0].shape[1] * initial_x_offset_fraction)
+    for values in values_list[1:]:
+        result = _stich(result, values, result.shape[1] - x_offset_right)
+    return result
 
 
 # get rms roughness
-def nanrms(x, axis=None):
+def nanrms(x: np.ndarray, axis=None):
+    """Returns root mean square of given numpy.ndarray x."""
     return np.sqrt(np.nanmean(x**2, axis=axis))
 
 

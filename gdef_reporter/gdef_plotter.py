eb5037c4c6b3f35f1fbb9949e468e7d0daa2c7e4
natter1 <njoehrmann@gmail.com>
2020-08-17 20:18:47 +0200

    update

--- gdef_reporter/gdef_plotter.py
+++ gdef_reporter/gdef_plotter.py
@@ -10,9 +10,16 @@ from gdef_reader.utils import create_xy_rms_data, create_absolute_gradient_array
 
 
 class GDEFPlotter:
-    def __init__(self, figure_size=(12, 6), dpi=300):
+    def __init__(self, figure_size=(12, 6), dpi=300, auto_show=False):
+        """
+
+        :param figure_size:
+        :param dpi:
+        :param auto_show: automatically call figure.show(), when a figure is created
+        """
         self.dpi = dpi
         self.figure_size = figure_size
+        self.auto_show = auto_show
 
     def create_surface_figure(self, values: np.ndarray, pixel_width, cropped=True) -> Optional[Figure]:
         if values is None:
@@ -22,12 +29,49 @@ class GDEFPlotter:
         self.plot_surface_to_axes(ax, values, pixel_width)
         figure_max.tight_layout()
         if not cropped:
+            self._auto_show_figure(figure_max)
             return figure_max
 
         tight_bbox = figure_max.get_tightbbox(figure_max.canvas.get_renderer())
         figure_tight, ax = plt.subplots(figsize=tight_bbox.size, dpi=self.dpi)
         self.plot_surface_to_axes(ax, values, pixel_width)
+        self._auto_show_figure(figure_tight)
         return figure_tight
+
+    def create_rms_per_column_figure(self, values: np.ndarray, pixel_width, title=None, moving_average_n=1) -> Figure:
+        """
+        :param values: 2D array
+        :param pixel_width: in meter
+        :param title: optional figure title
+        :param moving_average_n: number of columns for moving average
+        :return: matplotlib Figure
+        """
+        x_pos, y_rms = create_xy_rms_data(values, pixel_width, moving_average_n)
+        result, (ax_rms) = plt.subplots(1, 1, figsize=self.figure_size, dpi=self.dpi)
+        ax_rms.plot(x_pos, y_rms, 'r')
+        ax_rms.set_xlabel("[µm]")
+        ax_rms.set_ylabel(f"root mean square (moving average over {moving_average_n} column(s))")
+        if title:
+            result.suptitle(f'{title}', fontsize=16)
+        self._auto_show_figure(result)
+        return result
+
+    def create_absolute_gradient_rms_figure(self, values: np.ndarray, cutoff_percent_list, pixel_width, moving_average_n=1) -> Figure:
+        result, (ax_gradient_rms) = plt.subplots(1, 1, figsize=self.figure_size)
+        ax_gradient_rms.set_xlabel("[µm]")
+        ax_gradient_rms.set_ylabel(f"absolute gradient root mean square (moving average over {moving_average_n} column(s))")
+
+        for i, percent in enumerate(cutoff_percent_list):
+            absolut_gradient_array = create_absolute_gradient_array(values, percent / 100.0)
+            x_pos, y_gradient_rms = create_xy_rms_data(absolut_gradient_array, pixel_width, moving_average_n)
+            ax_gradient_rms.plot(x_pos, y_gradient_rms, label=f"{percent}%")
+        ax_gradient_rms.legend()
+        self._auto_show_figure(result)
+        return result
+
+    def _auto_show_figure(self, fig):
+        if self.auto_show:
+            fig.show()
 
     @classmethod
     def plot_surface_to_axes(cls, ax: Axes, values: np.ndarray, pixel_width: float):
@@ -50,32 +94,3 @@ class GDEFPlotter:
         cax.set_title("nm", y=1)  # bar.set_label("nm")
         plt.colorbar(im, cax=cax)
 
-    @classmethod
-    def create_rms_per_column_figure(cls, values: np.ndarray, pixel_width, title=None, moving_average_n=1) -> Figure:
-        """
-        :param values: 2D array
-        :param pixel_width: in meter
-        :param title: optional figure title
-        :param moving_average_n: number of columns for moving average
-        :return: matplotlib Figure
-        """
-        x_pos, y_rms = create_xy_rms_data(values, pixel_width, moving_average_n)
-        result, (ax_rms) = plt.subplots(1, 1, figsize=(10, 6))
-        ax_rms.plot(x_pos, y_rms, 'r')
-        ax_rms.set_xlabel("[µm]")
-        ax_rms.set_ylabel(f"root mean square (moving average over {moving_average_n} column(s))")
-        if title:
-            result.suptitle(f'{title}', fontsize=16)
-        return result
-
-    def create_absolute_gradient_rms_figure(self, values: np.ndarray, cutoff_percent_list, pixel_width, moving_average_n=1) -> Figure:
-        result, (ax_gradient_rms) = plt.subplots(1, 1, figsize=(10, 10))
-        ax_gradient_rms.set_xlabel("[µm]")
-        ax_gradient_rms.set_ylabel(f"absolute gradient root mean square (moving average over {moving_average_n} column(s))")
-
-        for i, percent in enumerate(cutoff_percent_list):
-            absolut_gradient_array = create_absolute_gradient_array(values, percent / 100.0)
-            x_pos, y_gradient_rms = create_xy_rms_data(absolut_gradient_array, pixel_width, moving_average_n)
-            ax_gradient_rms.plot(x_pos, y_gradient_rms, label=f"{percent}%")
-        ax_gradient_rms.legend()
-        return result
